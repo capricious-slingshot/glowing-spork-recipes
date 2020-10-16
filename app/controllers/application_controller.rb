f3756a63c83a3a10add1fc9e130a53349cada682
capricious-slingshot <ephemere@protonmail.com>
2020-10-16 12:42:41 -0700

    fixing broken tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user?
 
   def set_nav_variable
-    @catagories = Category.all
+    @categories = Category.all
   end
 
   private
