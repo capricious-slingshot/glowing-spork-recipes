245b4d048f97d84444fe4c8c91d9802c44ff51a6
capricious-slingshot <ephemere@protonmail.com>
2020-10-21 12:46:52 -0700

    a user can edit their setings, and user info is displayed on show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,16 @@ module UsersHelper
     end
   end
 
+  def user_image(user)
+    user.photo.present? ? "users/#{user.photo}" : "https://bulma.io/images/placeholders/128x128.png" 
+  end
+
+  def user_bio(user)
+    user.bio.present? ? user.bio : "Say something about yourself ..." 
+  end
+
+  def user_location(user)
+    user.location.present? ? user.location : "Where do you live?"
+  end
+
 end
